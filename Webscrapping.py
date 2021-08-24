37941c4ce953d3cfb8dc9b093bffc43c20a60353
st20acv <sweettugz@gmail.com>
2021-08-24 16:07:56 +0100

    WEbscrapping the youtube page

---
+++ Webscrapping.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+url='https://www.youtube.com/c/KalleHallden/videos'
+browser=webdriver.Chrome()
+browser.get(url)
+browser.find_element_by_xpath('//*[@id="video-title"]').click()
